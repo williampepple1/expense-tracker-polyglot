1ff2edd38246ae279fcb0832520c7b20bf25c0eb
William Pepple <williampepple1@gmail.com>
2024-02-08 12:56:23 +0100

    Update routes.go
    Update with expense

--- golang/routes/routes.go
+++ golang/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"expense-tracker/handlers"
+	"expense-tracker/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,4 +12,16 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 
 	r.POST("/register", handlers.RegisterUser(db))
 	r.POST("/login", handlers.LoginUser(db))
+
+	// Use the authorization middleware for the following routes
+	authorized := r.Group("/")
+	authorized.Use(middleware.Authorize())
+	{
+		authorized.GET("/expenses", handlers.ListExpenses(db))
+		authorized.GET("/expenses/user/:userId", handlers.ListUserExpenses(db))
+		authorized.POST("/expenses", handlers.CreateExpense(db))
+		authorized.GET("/expenses/:id", handlers.GetExpense(db))
+		authorized.PUT("/expenses/:id", handlers.UpdateExpense(db))
+		authorized.DELETE("/expenses/:id", handlers.DeleteExpense(db))
+	}
 }

f8275fbc80b0f7ddb1c3c74233af762702d34740
William Pepple <williampepple1@gmail.com>
2024-02-08 09:57:14 +0100

    Update database.go

--- golang/config/database.go
+++ golang/config/database.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -26,14 +28,34 @@ func InitDB() (*gorm.DB, error) {
 	sslmode := "disable"
 	port := os.Getenv("DATABASE_PORT")
 	if port == "" {
-		port = "5432" // default to 5432 if not specified
+		port = "5432" // Default to 5432 if not specified
 	}
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s", host, port, user, dbname, sslmode, password)
-
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	// Connect to PostgreSQL without specifying the database name to check for its existence
+	defaultConnStr := fmt.Sprintf("host=%s port=%s user=%s sslmode=%s password=%s", host, port, user, sslmode, password)
+	db, err := sql.Open("postgres", defaultConnStr)
 	if err != nil {
 		return nil, err
 	}
-	return db, nil
+	defer db.Close()
+
+	// Check if the database exists
+	var exists int
+	db.QueryRow("SELECT 1 FROM pg_database WHERE datname=$1", dbname).Scan(&exists)
+
+	// If the database does not exist, create it
+	if exists == 0 {
+		_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbname))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Now, connect to the newly created or verified existing database using GORM
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s", host, port, user, dbname, sslmode, password)
+	gormDB, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return gormDB, nil
 }

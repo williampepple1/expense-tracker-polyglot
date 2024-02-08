4db406983c2683c79d61f18e441e8f92d177e53b
William Pepple <williampepple1@gmail.com>
2024-02-08 09:39:08 +0100

    Create routes.go

---
+++ golang/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"expense-tracker/handlers"
+	"expense-tracker/middleware"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(r *gin.Engine, db *gorm.DB) {
+
+	r.POST("/register", handlers.RegisterUser(db))
+	r.POST("/login", handlers.LoginUser(db))
+}
